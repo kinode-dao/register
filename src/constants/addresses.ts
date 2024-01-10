21c3ea0b92733b1bc9f386e756c71651fa4555db
dr-frmr <docterformer@protonmail.com>
2024-01-10 18:32:28 -0300

    add new contract addresses for sepolia and optimism

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -3,11 +3,11 @@ import { ChainId } from './chainId'
 type AddressMap = { [chainId: string]: string }
 
 export const NDNS_REGISTRY_ADDRESSES: AddressMap = {
-    [ChainId.SEPOLIA]: '0x6e22E7b9f5a99D5921c14A88Aaf954502aC17B90',
-    [ChainId.OPTIMISM]: '0x0',
+    [ChainId.SEPOLIA]: '0xa11e3794e701565aD37f84DD364d581f5e9518c9',
+    [ChainId.OPTIMISM]: '0x942A69Cc3dd5d9a87c35f13ebA444adc00934B0F',
 }
 
 export const DOT_NEC_ADDRESSES: AddressMap = {
-    [ChainId.SEPOLIA]: '0x09eba32998888032CF4f806E5fb7A48932aE7022',
-    [ChainId.OPTIMISM]: '0x0',
+    [ChainId.SEPOLIA]: '0x872Ac3454f6834f8BBC79F96d3D749EE6F8fE913',
+    [ChainId.OPTIMISM]: '0x7fccd23e47a4830b0a59df30624f3d1dab863e7c',
 }
